2a2a6ebe9aa846aa4e3611bdc89460af9f9e9036
l3gacyb3ta <l3gacy.b3ta@disroot.org>
2021-07-09 20:54:55 -0400

    Working script

--- zeph_cont.cr
+++ zeph_cont.cr
@@ -1,23 +1,28 @@
-# require "csv"
-
+require "csv"
 
 # Builds the XML of a given contact data.
 # Example:
 # build_contact("Arc", "l3g@shoe.com", "123-456-7890", "12345")
-def build_contact(contact_name : String, contact_email : String, contact_phone : String, contact_zip : String)
-  string = "BEGIN:VCARD
-  VERSION:2.1
-  FN:#{contact_name}
-  EMAIL;X-INTERNET:#{contact_email}
-  TEL;CELL:#{contact_phone}
-  END:VCARD
-  "
+def build_contact(contact_name : String, contact_email : String, contact_phone : String, contact_zip : String) : String
+  string = "
+BEGIN:VCARD
+VERSION:2.1
+N:;#{contact_name};
+FN:#{contact_name}
+EMAIL;X-INTERNET:#{contact_email}
+TEL;CELL:#{contact_phone}
+END:VCARD"
   string
 end
 
-vcard_gen = build_contact("Arc", "l3g@shoe.com", "123-456-7890", "12345")
+# An empty string of the contact data.
+contacts = ""
 
-puts vcard_gen
+csv = CSV.new File.open("zephyr.csv"), headers: true
+while csv.next
+    contacts = contacts + build_contact csv["Name"], csv["Email Address"], csv["Phone #"], csv["Zip Code"]
+end
 
-# Writes the XML to a file.
-File.write "contacts.vcf", vcard_gen
+puts contacts
+
+File.write "contacts.vcf", contacts
